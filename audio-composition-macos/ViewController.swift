794139ba546589dbbae6e97fcd7210a2bf92ce64
russelldzhafarov <russell.dzhafarov@gmail.com>
2021-08-21 18:55:13 +0800

    Initial Commit

---
+++ audio-composition-macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  audio-composition-macos
+//
+//  Created by blessed on 21.08.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
