b6c7164c86510a32b93ac065acb825d56605ba62
russelldzhafarov <russell.dzhafarov@gmail.com>
2021-08-23 00:24:53 +0800

    Added observers to update UI and button actions

--- audio-composition-macos/ViewController.swift
+++ audio-composition-macos/ViewController.swift
@@ -2,13 +2,28 @@
 //  ViewController.swift
 //  audio-composition-macos
 //
-//  Created by blessed on 21.08.2021.
+//  Created by russell.dzhafarov@gmail.com on 21.08.2021.
 //
 
 import Cocoa
+import Combine
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var playButton: NSButton!
+    @IBOutlet weak var exportButton: NSButton!
+    @IBOutlet weak var overlayView: OverlayView!
+    @IBOutlet weak var timelineView: TimelineView!
+    @IBOutlet weak var rulerView: RulerView!
+    @IBOutlet weak var currentTimeLabel: NSTextField!
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    deinit {
+        cancellables.forEach{ $0.cancel() }
+        cancellables.removeAll()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,10 +32,102 @@ class ViewController: NSViewController {
 
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            guard let timeline = representedObject as? Timeline else { return }
+            
+            timeline.$tracks
+                .receive(on: DispatchQueue.main)
+                .sink { [weak self] newValue in
+                    self?.timelineView.needsDisplay = true
+                    self?.exportButton.isEnabled = !newValue.isEmpty
+                }
+                .store(in: &cancellables)
+            
+            timeline.$highlighted
+                .receive(on: DispatchQueue.main)
+                .sink { [weak self] newValue in
+                    self?.overlayView.needsDisplay = true
+                }
+                .store(in: &cancellables)
+            
+            timeline.$currentTime
+                .receive(on: DispatchQueue.main)
+                .sink { [weak self] newValue in
+                    self?.overlayView.needsDisplay = true
+                    self?.currentTimeLabel.stringValue = newValue.hhmmssms()
+                }
+                .store(in: &cancellables)
+            
+            timeline.$selectedTimeRange
+                .receive(on: DispatchQueue.main)
+                .sink { [weak self] newValue in
+                    self?.overlayView.needsDisplay = true
+                }
+                .store(in: &cancellables)
+            
+            timeline.$visibleTimeRange
+                .receive(on: DispatchQueue.main)
+                .sink { [weak self] _ in
+                    self?.rulerView.needsDisplay = true
+                    self?.timelineView.needsDisplay = true
+                    self?.overlayView.needsDisplay = true
+                }
+                .store(in: &cancellables)
+            
+            timeline.$playerState
+                .receive(on: DispatchQueue.main)
+                .sink { [weak self] newValue in
+                    switch newValue {
+                    case .playing:
+                        self?.playButton.image = NSImage(systemSymbolName: .pause, accessibilityDescription: "")
+                    case .stopped:
+                        self?.playButton.image = NSImage(systemSymbolName: .play, accessibilityDescription: "")
+                    }
+                }
+                .store(in: &cancellables)
+            
+            timeline.$error
+                .receive(on: DispatchQueue.main)
+                .sink { newValue in
+                    if let newValue = newValue {
+                        let alert = NSAlert()
+                        alert.alertStyle = .critical
+                        alert.messageText = "Something went wrong!"
+                        alert.informativeText = newValue.localizedDescription
+                        alert.runModal()
+                    }
+                }
+                .store(in: &cancellables)
         }
     }
-
-
+    
+    // MARK: - Actions
+    override func selectAll(_ sender: Any?) {
+        guard let timeline = representedObject as? Timeline else { return }
+        timeline.selectedTimeRange = 0.0 ..< timeline.duration
+    }
+    @IBAction func actionBackwardEnd(_ sender: Any) {
+        guard let timeline = representedObject as? Timeline else { return }
+        timeline.backwardEnd()
+    }
+    @IBAction func actionBackward(_ sender: Any) {
+        guard let timeline = representedObject as? Timeline else { return }
+        timeline.backward()
+    }
+    @IBAction func actionPlay(_ sender: Any) {
+        guard let timeline = representedObject as? Timeline else { return }
+        timeline.play()
+    }
+    @IBAction func actionForward(_ sender: Any) {
+        guard let timeline = representedObject as? Timeline else { return }
+        timeline.forward()
+    }
+    @IBAction func actionForwardEnd(_ sender: Any) {
+        guard let timeline = representedObject as? Timeline else { return }
+        timeline.forwardEnd()
+    }
+    @IBAction func actionExport(_ sender: Any) {
+        guard let timeline = representedObject as? Timeline else { return }
+        timeline.export()
+    }
 }
 
