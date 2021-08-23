c78fbe6a155ddc5191345e860ca8832921a2dddd
russelldzhafarov <russell.dzhafarov@gmail.com>
2021-08-24 01:48:18 +0800

    Added window controller

---
+++ audio-composition-macos/WindowController.swift
@@ -0,0 +1,16 @@
+//
+//  WindowController.swift
+//  audio-composition-macos
+//
+//  Created by russell.dzhafarov@gmail.com on 21.08.2021.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        window?.backgroundColor = NSColor.windowBackgroundColor
+    }
+}
