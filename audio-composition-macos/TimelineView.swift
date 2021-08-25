6def5da8e66a71b32dc5b47eb2605607ed86bf39
russelldzhafarov <russell.dzhafarov@gmail.com>
2021-08-25 10:35:44 +0800

    Implemented moving assets in timeline

--- audio-composition-macos/TimelineView.swift
+++ audio-composition-macos/TimelineView.swift
@@ -16,6 +16,85 @@ class TimelineView: NSView {
     override var isFlipped: Bool { true }
     override var mouseDownCanMoveWindow: Bool { false }
     
+    // MARK: - Events
+    override func scrollWheel(with event: NSEvent) {
+        nextResponder?.scrollWheel(with: event)
+        
+        guard let timeline = timeline else { return }
+        
+        let duration = timeline.visibleDur
+        let secPerPx = CGFloat(duration) / bounds.width
+        
+        let deltaPixels = event.deltaX < 0
+            ? min(-event.deltaX * secPerPx,
+                  CGFloat(timeline.duration - timeline.visibleTimeRange.upperBound))
+            : min(event.deltaX * secPerPx,
+                  CGFloat(timeline.visibleTimeRange.lowerBound)) * -1
+        
+        if deltaPixels != 0 {
+            timeline.visibleTimeRange = timeline.visibleTimeRange.lowerBound + Double(deltaPixels) ..< timeline.visibleTimeRange.upperBound + Double(deltaPixels)
+        }
+    }
+    override func mouseDown(with event: NSEvent) {
+        guard let timeline = timeline,
+              !timeline.isEmpty else { return }
+        
+        // Clear selection
+        timeline.tracks.forEach{ $0.asset.isSelected = false }
+        timeline.needsDisplay = true
+        
+        let start = convert(event.locationInWindow, from: nil)
+        let oneSecWidth = bounds.width / CGFloat(timeline.visibleDur)
+        
+        var selected: AudioAsset?
+        var y: CGFloat = .zero
+        for track in timeline.tracks {
+            let assetRect = CGRect(x: CGFloat(track.asset.startTime - timeline.visibleTimeRange.lowerBound) * oneSecWidth,
+                                   y: y,
+                                   width: CGFloat(track.asset.duration) * oneSecWidth - CGFloat(4),
+                                   height: timeline.trackHeight)
+            
+            if NSPointInRect(start, assetRect) {
+                track.asset.isSelected = true
+                selected = track.asset
+                timeline.needsDisplay = true
+                break
+            }
+            
+            y += timeline.trackHeight
+        }
+        
+        if let selected = selected {
+            move(asset: selected, with: event)
+        }
+    }
+    
+    func move(asset: AudioAsset, with event: NSEvent) {
+        guard let timeline = timeline,
+              !timeline.isEmpty else { return }
+        
+        let start = convert(event.locationInWindow, from: nil)
+        
+        let assetStartTime = asset.startTime
+        while true {
+            guard let nextEvent = window?.nextEvent(matching: [.leftMouseUp, .leftMouseDragged]) else { continue }
+            
+            let end = convert(nextEvent.locationInWindow, from: nil)
+            
+            if Int(start.x) != Int(end.x) {
+                let oneSecWidth = bounds.width / CGFloat(timeline.visibleDur)
+                let change = TimeInterval((end.x - start.x) / oneSecWidth)
+                
+                asset.startTime = max(0, (assetStartTime + change))
+                timeline.needsDisplay = true
+            }
+            
+            if nextEvent.type == .leftMouseUp {
+                break
+            }
+        }
+    }
+    
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
         guard let timeline = timeline,
@@ -58,11 +137,10 @@ class TimelineView: NSView {
             // Draw asset selection
             if track.asset.isSelected {
                 let frame = CGRect(x: CGFloat(track.asset.startTime - timeline.visibleTimeRange.lowerBound) * oneSecWidth,
-                                   y: y,
+                                   y: y + CGFloat(2),
                                    width: CGFloat(track.asset.duration) * oneSecWidth,
-                                   height: timeline.trackHeight)
-                    .insetBy(dx: CGFloat(1), dy: CGFloat(1))
-                ctx.setStrokeColor(NSColor.timelineWaveColor.cgColor)
+                                   height: timeline.trackHeight - CGFloat(4))
+                ctx.setStrokeColor(NSColor.systemTeal.cgColor)
                 ctx.setLineWidth(CGFloat(2))
                 ctx.stroke(frame)
             }
@@ -96,11 +174,11 @@ class TimelineView: NSView {
         
         // Draw asset visible rect
         let frame = CGRect(x: CGFloat(timeRange.lowerBound - timeline.visibleTimeRange.lowerBound) * oneSecWidth,
-                           y: origin.y,
+                           y: origin.y + CGFloat(2),
                            width: CGFloat(timeRange.upperBound - timeRange.lowerBound) * oneSecWidth,
-                           height: timeline.trackHeight)
+                           height: timeline.trackHeight - CGFloat(4))
         
-        ctx.setFillColor(NSColor.timelineWaveBackgroundColor.cgColor)
+        ctx.setFillColor(asset.isSelected ? NSColor.systemTeal.withAlphaComponent(0.6).cgColor : NSColor.timelineWaveBackgroundColor.cgColor)
         ctx.fill(frame)
         
         
