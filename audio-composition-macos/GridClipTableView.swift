0f1f3b6db51ff0fc71c2e52b582c6bc22359da14
russelldzhafarov <russell.dzhafarov@gmail.com>
2021-09-08 13:00:57 +0800

    Added GridClipTableView

---
+++ audio-composition-macos/GridClipTableView.swift
@@ -0,0 +1,12 @@
+//
+//  GridClipTableView.swift
+//  audio-composition-macos
+//
+//  Created by russell.dzhafarov@gmail.com on 07.09.2021.
+//
+
+import Cocoa
+
+class GridClipTableView: NSTableView {
+    override func drawGrid(inClipRect clipRect: NSRect) {}
+}
