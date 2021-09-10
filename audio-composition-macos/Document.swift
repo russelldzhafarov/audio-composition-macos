7b5076fa81264e8a9b922d713ce86a449e203670
russelldzhafarov <russell.dzhafarov@gmail.com>
2021-09-10 19:04:37 +0800

    Fixed document reading and writing

--- audio-composition-macos/Document.swift
+++ audio-composition-macos/Document.swift
@@ -6,6 +6,7 @@
 //
 
 import Cocoa
+import AVFoundation
 
 extension NSStoryboard.Name {
     static let main = NSStoryboard.Name("Main")
@@ -17,35 +18,68 @@ extension NSStoryboard.SceneIdentifier {
 
 class Document: NSDocument {
     
+    // Model
     let timeline = Timeline(tracks: [AudioTrack(id: UUID(), name: "Channel # 1", assets: [])],
+                            windowController: nil,
                             undoManager: nil)
     
-    override init() {
-        super.init()
-        // Add your subclass-specific initialization here.
-    }
-
-    override class var autosavesInPlace: Bool {
-        return true
-    }
-
+    override class var preservesVersions: Bool { false }
+    override class var autosavesInPlace: Bool { false }
+    
+    // Top-level document wrapper
+    var documentFileWrapper: FileWrapper?
+    
     override func makeWindowControllers() {
         // Returns the Storyboard that contains your Document window.
         let storyboard = NSStoryboard(name: .main, bundle: nil)
-        let windowController = storyboard.instantiateController(withIdentifier: .documentWindowController) as! NSWindowController
-        self.addWindowController(windowController)
-        
-        timeline.undoManager = windowController.window?.undoManager
-        
-        windowController.contentViewController?.representedObject = timeline
+        if let windowController = storyboard.instantiateController(withIdentifier: .documentWindowController) as? EditorWindowController {
+            
+            addWindowController(windowController)
+            
+            timeline.fileUrl = fileURL
+            timeline.windowController = windowController
+            timeline.undoManager = windowController.window?.undoManager
+            
+            windowController.contentViewController?.representedObject = timeline
+        }
     }
     
-    override func data(ofType typeName: String) throws -> Data {
-        return try JSONEncoder().encode(timeline.tracks)
-    }
+    override class func canConcurrentlyReadDocuments(ofType typeName: String) -> Bool { true }
+    
+    override func read(from fileWrapper: FileWrapper, ofType typeName: String) throws {
+        guard fileWrapper.isDirectory,
+              let fileWrappers = fileWrapper.fileWrappers,
+              let docWrapper = fileWrappers["data.json"],
+              let jsonData = docWrapper.regularFileContents
+        else {
+            throw NSError(domain: "com.russelldzhafarov.audio-composition-macos",
+                          code: 0,
+                          userInfo: [NSLocalizedDescriptionKey: "Project file is broken"])
+        }
 
-    override func read(from data: Data, ofType typeName: String) throws {
-        timeline.tracks = try JSONDecoder().decode([AudioTrack].self, from: data)
+        timeline.tracks = try JSONDecoder().decode([AudioTrack].self, from: jsonData)
+        
+        timeline.fileUrl = fileURL
+        
+        self.documentFileWrapper = fileWrapper
+    }
+    
+    override func canAsynchronouslyWrite(to url: URL, ofType typeName: String, for saveOperation: NSDocument.SaveOperationType) -> Bool { true }
+    
+    override func fileWrapper(ofType typeName: String) throws -> FileWrapper {
+        
+        if documentFileWrapper == nil {
+            documentFileWrapper = FileWrapper(directoryWithFileWrappers: [:])
+        }
+
+        let jsonData = try JSONEncoder().encode(timeline.tracks)
+        if let fileWrapper = documentFileWrapper?.fileWrappers?["data.json"] {
+            documentFileWrapper?.removeFileWrapper(fileWrapper)
+        }
+        
+        documentFileWrapper?.addRegularFile(withContents: jsonData, preferredFilename: "data.json")
+        
+        return documentFileWrapper!
     }
 }
 
